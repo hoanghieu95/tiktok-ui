451edd1a2261c02730c812b31cde214c09f0913e
hoanghieu95 <vuhoanghieu95@gmail.com>
2022-09-09 22:11:55 +0700

    09/09/2022

---
+++ src/components/Image/index.js
@@ -0,0 +1,25 @@
+import classNames from 'classnames';
+import { useState, forwardRef } from 'react';
+import images from '~/assets/images';
+import styles from './Image.module.scss';
+
+const Image = forwardRef(({ fallback: customFallback= images.NoImage, className, src, alt, ...props }, ref) => {
+    const [fallback, setFallback] = useState('');
+
+    const handleError = () => {
+        setFallback(customFallback);
+    };
+
+    return (
+        <img
+            className={classNames(styles.wrapper, className)}
+            ref={ref}
+            src={fallback || src}
+            alt={alt}
+            {...props}
+            onError={handleError}
+        />
+    );
+});
+
+export default Image;
